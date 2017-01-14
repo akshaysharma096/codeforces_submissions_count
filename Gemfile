ecd95bdd769a8d878f382ee3bf918a4ed277adb4
Akshay Sharma <akshaysharma@Akshays-MacBook-Air.local>
2017-01-14 19:58:46 +0530

    initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "https://rubygems.org"
+
+gem 'mechanize'
+gem 'selenium-webdriver', '~> 2.53'
+gem 'nokogiri'
---
+++ script.rb
@@ -0,0 +1,21 @@
+require 'mechanize'
+
+
+options = ["google", "form"]
+begin
+  login_option = ARGV[0]
+  puts 'Invalid option' unless login_option
+  until options.include? login_option
+    puts 'Enter option: '
+    login_option = eval gets.chomp.downcase
+    break if options.include? login_option
+  end
+  agent = Mechanize.new
+  page = agent.get 'http://codeforces.com/enter'
+  if login_option.eql? options[0]
+    google_page = page.link_with(:class => 'useGmailLink').click
+    puts google_page
+  end
+rescue SocketError
+  puts 'Please connect to interent.'
+end
