5cf82ee08174923ec888367ab6c39743d3fdaab9
Akshay Sharma <akshaysharma@Akshays-MacBook-Air.local>
2017-01-14 21:31:36 +0530

    final commit

--- script.rb
+++ script.rb
@@ -13,11 +13,22 @@ begin
     handle = gets.chomp.downcase
     break if handle
   end
-  handle_link = "http://codeforces.com/submissions/#{handle}"
+  handle_link = "http://codeforces.com/submissions/#{handle}/"
   agent = Mechanize.new
   agent.redirect_ok = !1
   page = agent.get handle_link
   exit_prog if page.code == "302"
+  problem_count ||= page.css('span.verdict-accepted').size
+  counter=page.css('span.page-index > a')[-1].text.to_i
+  page_count = 2
+  page_count.upto(counter).each do |page_count|
+    new_page = handle_link+"page/#{page_count}"
+    page = agent.get new_page
+    problem_count += page.css('span.verdict-accepted').size
+  end
+  puts "Your successfull problem submissions are: #{problem_count}"
+rescue NoMethodError
+  puts 'Error'
 rescue SocketError
   puts 'Please connect to internet.'
 end
