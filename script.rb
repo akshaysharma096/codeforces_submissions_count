64af02086431607502bba6425944d9d40e2ba8ca
Akshay Sharma <akshaysharma@Akshays-MacBook-Air.local>
2017-01-14 20:13:03 +0530

    initial code

--- script.rb
+++ script.rb
@@ -1,21 +1,22 @@
 require 'mechanize'
 
+def exit_prog
+  puts 'No handle'
+  exit
+end
 
-options = ["google", "form"]
 begin
-  login_option = ARGV[0]
-  puts 'Invalid option' unless login_option
-  until options.include? login_option
-    puts 'Enter option: '
-    login_option = eval gets.chomp.downcase
-    break if options.include? login_option
+  handle = ARGV[0]
+  puts 'Please specify a handle' unless handle
+  until options.include? handle
+    puts 'Enter a handle'
+    handle = eval gets.chomp.downcase
+    break if options.include? handle
   end
   agent = Mechanize.new
-  page = agent.get 'http://codeforces.com/enter'
-  if login_option.eql? options[0]
-    google_page = page.link_with(:class => 'useGmailLink').click
-    puts google_page
-  end
+  page = agent.get "http://codeforces.com/submissions/#{handle}"
+  code = page.code
+  exit_prog if code == 302
 rescue SocketError
   puts 'Please connect to internet.'
 end
